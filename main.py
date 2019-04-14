3422f98113403b0b9831ed5d8a63d55a449343a5
camelboat <camelboat@sjtu.edu.cn>
2019-04-15 01:50:23 +0800

    modify lib import sequence 	modified:   Bosch_Control_Panel_Cropped_1.jpg 	modified:   Bosch_Control_Panel_Cropped_2.jpg 	modified:   Bosch_Control_Panel_Cropped_3.jpg 	modified:   Bosch_Control_Panel_Cropped_4.jpg 	new file:   WIFI.png 	modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
+from urllib.request import urlopen, Request
+import requests
+import time
+#import json
 
 import platform
 if platform.node() == 'raspberrypi':
@@ -17,10 +21,7 @@ if use_device:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PIN, GPIO.OUT)
 
-from urllib.request import urlopen, Request
-import requests
-import json, time
-
+auto_upload_trigger = time.time()
 
 # light 1 related variables
 light_1_initialize_flag = 0
@@ -181,48 +182,64 @@ class Application(tk.Frame):
         self.img.pack()
 
     def read_instruction(self):
-        request = Request('https://api.thingspeak.com/talkbacks/31641/commands/execute?api_key=NU6M3B6JB1Q3IR76')
-        response = urlopen(request)
-        command = response.read().decode()
-        '''
-        command code:
-        [device type(1)][control instruction(2)]
-        device type: 0 - lighting system
-                     1 - air-conditioner
-        000: light off
-        001: light on
-        1000: air-conditioner off
-        1010: air-conditioner on
-        11xx: air-conditioner set to xx degrees
-        '''
-        global waiting_status
-        if not command:
-            if waiting_status == 0:
-                waiting_status = 1
-                print("waiting for commands...")
-                pass
-            else:
-                pass
+        global light_1_not_running
+        global light_1_off
+        global auto_upload_trigger
+        global  light_1_running
+        global light_1_start
+        if (time.time() - auto_upload_trigger) > 60:
+            if light_1_status == 0:
+                light_1_not_running = time.time() - light_1_off
+                uploading_light_off_time()
+                light_1_off = time.time()
+            elif light_1_status == 1:
+                light_1_running = time.time() - light_1_start
+                uploading_light_running_time()
+                light_1_start = time.time()
+            auto_upload_trigger = time.time()
         else:
-            waiting_status = 0
-            if command[0] == '0':
-                decode_light(command[1:3])
-
-            elif command[0] == '1':
-                decode_ac(command[1:4])
-
+            request = Request('https://api.thingspeak.com/talkbacks/31641/commands/execute?api_key=NU6M3B6JB1Q3IR76')
+            response = urlopen(request)
+            command = response.read().decode()
+            '''
+            command code:
+            [device type(1)][control instruction(2)]
+            device type: 0 - lighting system
+                         1 - air-conditioner
+            000: light off
+            001: light on
+            1000: air-conditioner off
+            1010: air-conditioner on
+            11xx: air-conditioner set to xx degrees
+            '''
+            global waiting_status
+            if not command:
+                if waiting_status == 0:
+                    waiting_status = 1
+                    print("waiting for commands...")
+                    pass
+                else:
+                    pass
             else:
-                print("invalid instruction")
-        status_tmp = panel_status.get()
-        if status_tmp == 0:
-            self.render = ImageTk.PhotoImage(self.load_1)
-        elif status_tmp == 10:
-            self.render = ImageTk.PhotoImage(self.load_2)
-        elif status_tmp == 1:
-            self.render = ImageTk.PhotoImage(self.load_3)
-        else:
-            self.render = ImageTk.PhotoImage(self.load_4)
-        self.img.config(image=self.render)
+                waiting_status = 0
+                if command[0] == '0':
+                    decode_light(command[1:3])
+
+                elif command[0] == '1':
+                    decode_ac(command[1:4])
+
+                else:
+                    print("invalid instruction")
+            status_tmp = panel_status.get()
+            if status_tmp == 0:
+                self.render = ImageTk.PhotoImage(self.load_1)
+            elif status_tmp == 10:
+                self.render = ImageTk.PhotoImage(self.load_2)
+            elif status_tmp == 1:
+                self.render = ImageTk.PhotoImage(self.load_3)
+            else:
+                self.render = ImageTk.PhotoImage(self.load_4)
+            self.img.config(image=self.render)
 
     def updater(self):
         self.read_instruction()
